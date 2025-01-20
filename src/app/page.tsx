1446fe7478462b0bd867bf401813c02f72423799
Ugo Chinemerem <ugofranklin22@gmail.com>
2025-01-20 10:39:16 +0100

    metadata update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
       <p>Click for more info</p>
-      <a href="https://platformstage.decharge.network">
+      <a href="https://paj.cash">
         <IconLighter stroke={2} />
       </a>
     </main>
